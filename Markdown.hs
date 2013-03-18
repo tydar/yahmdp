1b6b7f49478628da79b524a91fee12bf8fd2b8d2
Tyler Darnell <tyler.a.darnell@gmail.com>
2013-03-18 15:08:56 -0500

    Added beginnings of block parsers.

--- Markdown.hs
+++ Markdown.hs
@@ -9,6 +9,7 @@ data Paragraph = Normal ParText
                | Quote [Paragraph]
                | UList [Paragraph]
                | OList [Paragraph]
+    deriving (Show)
 
 type ParText = [MText]
 
@@ -23,15 +24,32 @@ whitespace :: ReadP Char
 whitespace = satisfy ws
     where ws c = c `elem` [' ', '\t']
 
+oneOf :: [String] -> ReadP String
+oneOf xs = foldr1 (<++) $ map (string) xs
+
+specials :: [String]
+specials = ["*", "_", "`", "  \n"]
+
 -- parses markdown text which ends with a particular separator
 markdownText :: String -> ReadP String
+markdownText [] = do
+    consumed <- manyTill get (oneOf specials)
+    guard (consumed /= "")
+    return $ consumed
 markdownText end = do
     consumed <- manyTill get (string end) 
     guard (consumed /= "")
     return $ consumed
 
-parseSpans :: ReadP MText
-parseSpans = hardbreak +++ codeSpan +++ spanAsterisk +++ spanUnderscore
+parseMText :: ReadP MText
+parseMText = do
+    consumed <- markdownText []
+    guard (consumed /= "")
+    return $ MText consumed
+
+-- SPAN PARSING STARTS HERE --
+parseSpans :: ReadP ParText
+parseSpans = many1 (hardbreak +++ codeSpan +++ spanAsterisk +++ spanUnderscore +++ parseMText)
 
 hardbreak :: ReadP MText
 hardbreak = optional (whitespace) >> count 2 whitespace >> satisfy (=='\n') >> return Hardbreak
@@ -71,3 +89,32 @@ strongUnderscore = do
     string "__"
     content <- markdownText "__"
     return $ Strong content
+-- SPAN PARSING ENDS HERE --
+
+-- START BLOCK PARSING HERE --
+
+normalParagraph :: ReadP Paragraph
+normalParagraph = do
+    content <- parseSpans
+    optional (string "\n\n")
+    return $ Normal content
+
+indentedLineSpaces :: ReadP String
+indentedLineSpaces = do
+    string "   "
+    content <- manyTill get (char '\n')
+    return content
+
+indentedLineTab :: ReadP String
+indentedLineTab = do
+    char '\t'
+    content <- manyTill get (char '\n')
+    return content
+
+indentedLine :: ReadP String
+indentedLine = indentedLineSpaces +++ indentedLineTab
+
+preformattedBlock :: ReadP Paragraph
+preformattedBlock = do
+    lines <- many1 indentedLine
+    return $ Pre $ concat lines
